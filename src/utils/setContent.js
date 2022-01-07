c0e070e4a1113909b43b451be5a8bd9f1a8bbea9
Dima Tarasov <dmytrotarasov9@gmail.com>
2022-01-07 13:05:54 +0100

    make it better

--- src/utils/setContent.js
+++ src/utils/setContent.js
@@ -1,5 +1,5 @@
-import ErrorMessage from '../components/errorMessage/ErrorMessage';
 import Spinner from '../components/spinner/Spinner';
+import ErrorMessage from '../components/errorMessage/ErrorMessage';
 import Skeleton from '../components/skeleton/Skeleton';
 
 const setContent = (process, Component, data ) => {
@@ -8,10 +8,10 @@ const setContent = (process, Component, data ) => {
             return <Skeleton/>;
         case 'loading' :
             return <Spinner/>;
-        case 'error' :
-            return <ErrorMessage/>;
         case 'confirmed' :
             return <Component data={data}/>;
+        case 'error' :
+            return <ErrorMessage/>;
         default: 
             throw new Error('Unexpected process state'); 
     } 
